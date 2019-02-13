dd33f8e3639957cdac9f5dd2d5a87254318d1259
Rachel Ogden <rlogden@dmacc.edu>
2019-02-12 22:28:17 -0600

    Corrected after new info in slack

--- src/edu/dmacc/codedsm/hw4/VariableAdder.java
+++ src/edu/dmacc/codedsm/hw4/VariableAdder.java
@@ -2,13 +2,14 @@ package edu.dmacc.codedsm.hw4;
 
 public class VariableAdder {
 
-    final static int constantInteger = 5;
-    static int instanceInteger = 10;
+    final int constantInteger = 5;
+    int instanceInteger = 10;
 
     public static void main(String[] args) {
 
+        VariableAdder a = new VariableAdder();
         int methodInteger = 20;
-        int result = constantInteger + instanceInteger + methodInteger;
+        int result = a.constantInteger + a.instanceInteger + methodInteger;
         System.out.println(result);
 
     }
